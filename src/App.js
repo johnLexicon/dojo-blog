46a4478e70bb024d3c4dcda552d0c369e5ee002c
john lundgren <john.lundgren.lexicon@gmail.com>
2021-06-14 10:07:11 +0200

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import Home from './components/Home';
+import CreatePost from './components/CreatePost';
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/create">
+            <CreatePost />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
