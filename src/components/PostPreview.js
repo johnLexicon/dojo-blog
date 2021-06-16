259d0f8f3993dffd0db570b8ea22d2217a2e64b5
john lundgren <john.lundgren.lexicon@gmail.com>
2021-06-16 08:35:57 +0200

    Remove post functionality.

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,12 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { API } from '../shared/config.js';
+import { postsDataService } from '../shared/postsDataservice.js';
 
 const PostPreview = ({ postPreview, isPreview, isRemovable }) => {
+  const history = useHistory();
+
+  const onRemove = async () => {
+    const wasRemoved = await postsDataService.remove(API, postPreview._id);
+    if (wasRemoved) {
+      history.push('/');
+    }
+  };
+
   return (
     <div className="card mb-4">
       {isRemovable && (
         <i
           className="bi bi-x-square-fill position-absolute top-0 end-0 text-light h2"
           style={{ cursor: 'pointer' }}
+          onClick={onRemove}
         ></i>
       )}
       <img src={postPreview.imgUrl} className="card-img-top" alt="" />
